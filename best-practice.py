5ec572b9551b339b8f6caa38e0df230d8bb2a072
ubuntu <romarroc123a@gmail.com>
2023-08-31 00:46:37 +0400

    added wildfire profile

--- best-practice.py
+++ best-practice.py
@@ -185,7 +185,6 @@ def push_av_profile_to_palo_alto(ip, username, password):
     response = requests.post(url, headers=headers, params=params, json=json_data, verify=False)
     return response.json()
 
-##############################################################################################
 def push_spyware_profile_to_palo_alto(ip, username, password):
     json_data = load_json_from_file('config/best-practice-spyware.json')
     
@@ -202,7 +201,21 @@ def push_spyware_profile_to_palo_alto(ip, username, password):
     response = requests.post(url, headers=headers, params=params, json=json_data, verify=False)
     return response.json()
 
-##############################################################################################    
+def push_wildfire_profile_to_palo_alto(ip, username, password):
+    json_data = load_json_from_file('config/best-practice-wildfire.json')
+    
+    api_key = get_api_key(ip, username, password)
+    url = f'https://{ip}/restapi/v10.2/Objects/WildFireAnalysisSecurityProfiles'
+    
+    headers = {
+        "Content-Type": "application/json",
+        "X-PAN-KEY": api_key
+    }
+    
+    params = {'name': 'best-practice-wildfire', 'location': 'vsys', 'vsys': 'vsys1'}
+
+    response = requests.post(url, headers=headers, params=params, json=json_data, verify=False)
+    return response.json()
 
 def push_external_dynamic_ip(ip, username, password):
     json_data = load_json_from_file('config/external_dynamic_ip.json')
@@ -335,7 +348,6 @@ def main():
     else:
         print("Exiting without pushing anti-virus profile to Palo Alto.")
 
-#############################################################################################################
     print("""
 
     ╔════════════════════════════════════════════════════════════════════╗
@@ -352,7 +364,24 @@ def main():
     else:
         print("Exiting without pushing anti-spyware profile to Palo Alto.")
 
-#############################################################################################################
+
+    print("""
+
+    ╔════════════════════════════════════════════════════════════════════╗
+    ║ Please review the content of best-practice-wildfire.json           ║
+    ╚════════════════════════════════════════════════════════════════════╝
+
+    """)
+
+    push_av_profile = input("\nDo you want to push the wildfire profile to Palo Alto? (yes/no): ").lower()
+    if push_url_profile == 'yes':
+        #pa_ip, pa_username, pa_password = "192.168.8.45", "admin", "P@ssw0rd"
+        response = push_wildfire_profile_to_palo_alto(pa_ip, pa_username, pa_password)
+        print("\nResponse from Palo Alto for wildfire profile:\n", response)
+    else:
+        print("Exiting without pushing wildfire profile to Palo Alto.")
+
+   
 
     print("""
 
